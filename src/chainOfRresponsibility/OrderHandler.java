74f2f36eb692082f3857bfd8d232ab6bab16fda4
Adil Ormanov <>
2024-10-16 16:15:30 +0500

    fixed 0.1

--- src/chainOfRresponsibility/OrderHandler.java
+++ src/chainOfRresponsibility/OrderHandler.java
@@ -2,12 +2,11 @@ package chainOfRresponsibility;
 
 public abstract class OrderHandler {
     protected OrderHandler nextHandler;
-    protected int handlerPriority; // Поле для приоритета
+    protected int handlerPriority;
 
     public void setNextHandler(OrderHandler nextHandler) {
         this.nextHandler = nextHandler;
     }
 
-    // Метод для обработки заказа, который будет реализован в подклассах
     public abstract void handleOrder(String orderType, int orderPriority);
 }
